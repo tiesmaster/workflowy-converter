237fdf52bacbcaa10670f6ad2157cf27d2a09054
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-09-25 08:13:11 +0200

    Apply MudBlazor template to WorkflowyConverter.Wasm project

--- csharp/WorkflowyConverter.Wasm/Program.cs
+++ csharp/WorkflowyConverter.Wasm/Program.cs
@@ -3,10 +3,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 using WorkflowyConverter.Wasm;
 
+using MudBlazor.Services;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
